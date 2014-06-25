c193db5a0213c481360ea21ca8c8ae34930cede0
Paul Hoffman <nkuitse@nkuitse.com>
2014-06-25 12:17:57 +0530

    fix syntax error

--- lib/File/Feed/OCLC/WorldCat.pm
+++ lib/File/Feed/OCLC/WorldCat.pm
@@ -16,7 +16,7 @@ sub _file_instance {
     if ($arg{'from'} =~ m{^(?:.*/)?metacoll\.([^.]+)\.(new|updates|deletes)\.D(\d\d\d\d)(\d\d)(\d\d)\.T(\d\d)(\d\d)(\d\d)(?:\.(.+))?\.(\d)\.([a-z]+)$}) {
         # File name spec from http://www.oclc.org/content/dam/support/worldshare-metadata/retrieve_marc.pdf (retrieved 2014-06-25)
         $arg{'oclc-symbol'} ||= $1;
-        $arg{'purpose'}     ||= $2 eq 'new' ? ADD : $2 eq 'updates' : REPLACE : DELETE;
+        $arg{'purpose'}     ||= $2 eq 'new' ? ADD : $2 eq 'updates' ? REPLACE : DELETE;
         my ($Y, $m, $d, $H, $M, $S) = ($3, $4, $5, $6, $7, $8);
         $H = '00' if $H eq '24';  # Why oh why do they make this mistake??
         @arg{qw(date year month day hour minute second)} = ($Y.$m.$d.'T'.$H.$M.$S, $Y, $m, $d, $H, $M, $S);
