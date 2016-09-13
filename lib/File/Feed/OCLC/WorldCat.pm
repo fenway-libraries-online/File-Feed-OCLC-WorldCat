6fe4ba9abc9e7f9600c02f837b4dd8c6e2985cf8
Paul Hoffman <paul@flo.org>
2016-09-13 09:20:35 -0400

    tweak file name parsing

--- lib/File/Feed/OCLC/WorldCat.pm
+++ lib/File/Feed/OCLC/WorldCat.pm
@@ -13,13 +13,13 @@ use constant DELETE  => 'delete';
 
 sub _file_instance {
     my ($self, %arg) = @_;
-    if ($arg{'path'} =~ m{^(?:.*/)?metacoll\.([^.]+)\.(new|updates|deletes)\.D(\d\d\d\d)(\d\d)(\d\d)\.T(\d\d)(\d\d)(\d\d)(?:\.(.+))?\.(\d)\.([a-z]+)$}) {
+    if ($arg{'path'} =~ m{^(?:.*/)?metacoll\.([^.]+)\.(new|updates|deletes)\.[DWMQ](\d\d\d\d)(\d\d)(\d\d)\.T(\d\d)(\d\d)(\d\d)(?:\.(.+))?\.(\d)\.([a-z]+)$}) {
         # File name spec from http://www.oclc.org/content/dam/support/worldshare-metadata/retrieve_marc.pdf (retrieved 2014-06-25)
         $arg{'oclc-symbol'} ||= $1;
         $arg{'purpose'}     ||= $2 eq 'new' ? ADD : $2 eq 'updates' ? REPLACE : DELETE;
         my ($Y, $m, $d, $H, $M, $S) = ($3, $4, $5, $6, $7, $8);
         $H = '00' if $H eq '24';  # Why oh why do they make this mistake??
-        @arg{qw(date year month day hour minute second)} = ($Y.$m.$d.'T'.$H.$M.$S, $Y, $m, $d, $H, $M, $S);
+        @arg{qw(date time year month day hour minute second)} = ($Y.$m.$d.'T'.$H.$M.$S, $H.$M.$S, $Y, $m, $d, $H, $M, $S);
         $arg{'collection'}  ||= $9 if defined $9;
         $arg{'digit'}       ||= $10;
         $arg{'mime-type'}   ||= $11 eq 'mrc' ? 'application/marc' : $11 eq 'xml' ? 'text/xml' : 'application/octet-stream';
